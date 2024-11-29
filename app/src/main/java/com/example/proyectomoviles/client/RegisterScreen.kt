100f4e9460aac992fa27041ab4a534de0aebb5f4
OriónZYX <cristian_cris85@outlook.com>
2024-11-28 21:27:44 -0600

    Register

--- app/src/main/java/com/example/proyectomoviles/client/RegisterScreen.kt
+++ app/src/main/java/com/example/proyectomoviles/client/RegisterScreen.kt
@@ -1,4 +1,12 @@
 package com.example.proyectomoviles.client
 
-class RegisterScreen {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectomoviles.R
+
+class RegisterScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+    }
 }
